e8999f188e4e45cdd261f3fcdd497766235d001d
Andrew Ferrari <andrew.ferrari@sptech.school>
2024-06-17 21:17:13 -0300

    add tests valid json

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,10 +2,45 @@
 const { test, expect } = require('@playwright/test');
 
 test('consult registered reservations', async ({ request }) => {
-  const response = await request.get('https://restful-booker.herokuapp.com/booking');
+  const response = await request.get('');
 
   console.log(await response.json());
 
   expect(response.ok()).toBeTruthy();
-  expect(response.status()).toBe(200)
+  expect(response.status()).toBe(200);
+});
+
+test('consult registered reservations by id', async ({ request }) => {
+  const response = await request.get('917');
+
+  const jsonBody = await response.json();
+  console.log(jsonBody);
+
+  expect(jsonBody.firstname).toBe('Josh');
+  expect(jsonBody.lastname).toBe('Allen');
+  expect(jsonBody.totalprice).toBe(111);
+  expect(jsonBody.depositpaid).toBeTruthy();
+  expect(jsonBody.bookingdates.checkin).toBe('2018-01-01');
+  expect(jsonBody.bookingdates.checkout).toBe('2019-01-01');
+  expect(jsonBody.additionalneeds).toBe('super bowls');
+
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
+});
+
+test('consult registered reservations by id valid fields', async ({ request }) => {
+  const response = await request.get('116');
+
+  const jsonBody = await response.json();
+  console.log(jsonBody);
+
+  expect(jsonBody).toHaveProperty('firstname');
+  expect(jsonBody).toHaveProperty('lastname');
+  expect(jsonBody).toHaveProperty('totalprice');
+  expect(jsonBody).toHaveProperty('depositpaid');
+  expect(jsonBody).toHaveProperty('bookingdates');
+  expect(jsonBody).toHaveProperty('additionalneeds');
+
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
 });
