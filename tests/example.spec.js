a9114791e4fdfefcd1dfee388913623b2ad13f56
Andrew Ferrari <andrew.ferrari@sptech.school>
2024-06-30 18:56:02 -0300

    new test valid update

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,8 +1,11 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+var tokenResponse;
+var id;
+
 test('consult registered reservations', async ({ request }) => {
-  const response = await request.get('');
+  const response = await request.get('booking');
 
   console.log(await response.json());
 
@@ -11,7 +14,7 @@ test('consult registered reservations', async ({ request }) => {
 });
 
 test('consult registered reservations by id', async ({ request }) => {
-  const response = await request.get('1662');
+  const response = await request.get('booking/1662');
 
   const jsonBody = await response.json();
   console.log(jsonBody);
@@ -29,7 +32,7 @@ test('consult registered reservations by id', async ({ request }) => {
 });
 
 test('consult registered reservations by id valid fields', async ({ request }) => {
-  const response = await request.get('116');
+  const response = await request.get('booking/116');
 
   const jsonBody = await response.json();
   console.log(jsonBody);
@@ -46,26 +49,26 @@ test('consult registered reservations by id valid fields', async ({ request }) =
 });
 
 test('create booking', async ({ request }) => {
-  const response = await request.post('', {
+  const response = await request.post('booking', {
     data: {
-      "firstname" : "Andrew",
-      "lastname" : "Test",
-      "totalprice" : 444,
-      "depositpaid" : true,
-      "bookingdates" : {
-          "checkin" : "2018-01-01",
-          "checkout" : "2019-01-01"
+      "firstname": "Andrew",
+      "lastname": "Test",
+      "totalprice": 444,
+      "depositpaid": true,
+      "bookingdates": {
+        "checkin": "2018-01-01",
+        "checkout": "2019-01-01"
       },
-      "additionalneeds" : "Breakfast"
+      "additionalneeds": "Breakfast"
     }
-    
+
   });
   console.log(await response.json());
-  
+
 
   expect(response.ok()).toBeTruthy();
   expect(response.status()).toBe(200);
-  
+
   const jsonBody = await response.json();
 
   expect(jsonBody.booking.firstname).toBe('Andrew');
@@ -75,5 +78,54 @@ test('create booking', async ({ request }) => {
   expect(jsonBody.booking.bookingdates.checkin).toBe('2018-01-01');
   expect(jsonBody.booking.bookingdates.checkout).toBe('2019-01-01');
   expect(jsonBody.booking.additionalneeds).toBe('Breakfast');
-  
+
+});
+
+test('update partial', async ({ request }) => {
+  const response = await request.post('auth', {
+    data: {
+      "username": "admin",
+      "password": "password123"
+    }
+  });
+  console.log(await response.json());
+
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
+
+  const responseBody = await response.json();
+  tokenResponse = responseBody.token;
+
+  const partialUpdateRequest =  await request.patch(`booking/20`, { 
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Cookie': `token=${tokenResponse}`
+    },
+    data: {
+      "firstname": "Test",
+      "lastname": "Retest",
+      "totalprice": 222,
+      "depositpaid": false,
+      "bookingdates": {
+        "checkin": "2020-01-01",
+        "checkout": "2021-01-01"
+      },
+      "additionalneeds": "Lunch"
+    }
+  });
+
+  expect(partialUpdateRequest.ok()).toBeTruthy();
+  expect(partialUpdateRequest.status()).toBe(200);
+
+  const jsonBody = await partialUpdateRequest.json();
+
+  expect(jsonBody.firstname).toBe('Test');
+  expect(jsonBody.lastname).toBe('Retest');
+  expect(jsonBody.totalprice).toBe(222);
+  expect(jsonBody.depositpaid).toBeFalsy();
+  expect(jsonBody.bookingdates.checkin).toBe('2020-01-01');
+  expect(jsonBody.bookingdates.checkout).toBe('2021-01-01');
+  expect(jsonBody.additionalneeds).toBe('Lunch');
+
 });
